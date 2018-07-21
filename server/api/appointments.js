f0ef513d6abe4c91cfda739bc6a299ce713c3bf0
Tyler Hsu <tyler.l.hsu@gmail.com>
2018-07-20 22:56:07 -0500

    fix bug where appointment routes would return wrong status codes

--- server/api/appointments.js
+++ server/api/appointments.js
@@ -5,34 +5,34 @@ export default Router()
   .get('/', async (req, res) => {
     const result = await Api.Appointment.get();
     res
-      .status(result.error ? 200 : 500)
+      .status(result.error ? 500 : 200)
       .send(result);
   })
 
   .get('/:id', async (req, res) => {
     const result = await Api.Appointment.get(req.params.id);
     res
-      .status(result.error ? 200 : 500)
+      .status(result.error ? 500 : 200)
       .send(result);
   })
 
   .post('/', async (req, res) => {
     const result = await Api.Appointment.create(req.body);
     res
-      .status(result.error ? 200 : 500)
+      .status(result.error ? 500 : 200)
       .send(result);
   })
 
   .put('/:id', async (req, res) => {
     const result = await Api.Appointment.update(req.params.id, req.body);
     res
-      .status(result.error ? 200 : 500)
+      .status(result.error ? 500 : 200)
       .send(result);
   })
 
   .delete('/:id', async (req, res) => {
     const result = await Api.Appointment.destroy(req.params.id);
     res
-      .status(result.error ? 200 : 500)
+      .status(result.error ? 500 : 200)
       .send(result);
   });
